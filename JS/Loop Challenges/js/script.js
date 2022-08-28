d9de6f75bd0179f9152e56deca1df684b5701c12
RiyadBusttami <Riyad.Bustami@gmail.com>
2022-08-28 22:21:03 +0300

    Initial Commit

---
+++ JS/Loop Challenges/js/script.js
@@ -0,0 +1,18 @@
+console.log("Challenge #1");
+for(let i=1;i<20;i+=2)console.log(i);
+
+console.log("Challenge #2");
+for(let i=100;i>=0;i--)if(i%3==0)console.log(i);
+
+console.log("Challenge #3");
+for(let i=4;i>=-3.5;i-=1.5)console.log(i);
+
+console.log("Challenge #4");
+let sum=0;
+for(let i=1;i<=100;i++)sum+=i;
+console.log(sum);
+
+console.log("Challenge #5");
+let fact=1;
+for(let i=1;i<=12;i++)fact*=i;
+console.log(fact);
