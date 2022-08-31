5fb169d0a37bf27b6dbc261501ee2c0c6e213c0f
RiyadBusttami <Riyad.Bustami@gmail.com>
2022-08-31 13:03:30 +0300

    Initial Commit

---
+++ JS/Making Connections/script.js
@@ -0,0 +1,37 @@
+console.log("page loaded...");
+
+function deleteItem(item)
+{
+    item.remove();
+}
+
+function decreaseConnectionReqs(){
+    let rqstsNumSpan = document.querySelector("#connections-rqsts-num");
+    let rqstsNum= parseInt(rqstsNumSpan.innerHTML);
+    rqstsNum--;
+    rqstsNumSpan.innerHTML= rqstsNum;
+}
+
+function increaseConnections(){
+    let conNumSpan = document.querySelector("#connections-num");
+    let conNum = parseInt(conNumSpan.innerHTML);
+    conNum++;
+    conNumSpan.innerHTML = conNum;
+}
+
+function accept(element){
+    deleteItem(element.parentElement.parentElement);
+    decreaseConnectionReqs();
+    increaseConnections();
+}
+
+function deny(element){
+    deleteItem(element.parentElement.parentElement);
+    decreaseConnectionReqs();
+}
+
+function changeName(){
+    let userNameSpan = document.querySelector(".card-body h1");
+    userNameSpan.textContent="any other name";
+}
+
